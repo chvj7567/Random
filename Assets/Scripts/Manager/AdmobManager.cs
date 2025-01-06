7f675681849a71a05201f1e27e222b68e8093ae7
chvj7567 <chvj7567@naver.com>
2025-01-06 21:34:44 +0900

    [AD] 광고 아이디 라이브 적용

--- Assets/Scripts/Manager/AdmobManager.cs
+++ Assets/Scripts/Manager/AdmobManager.cs
@@ -4,10 +4,11 @@ using System;
 
 public class AdmobManager : SingletoneStatic<AdmobManager>
 {
-    //string bannerAdUnitId = "";
+    //string bannerAdUnitId = "ca-app-pub-7085378387310828~5438733391";
     //string interstitialAdUnitId = "";
     //string rewardedAdUnitId = "";
 
+    //# Test
     string bannerAdUnitId = "ca-app-pub-3940256099942544/9214589741";
     string interstitialAdUnitId = "ca-app-pub-3940256099942544/1033173712";
     string rewardedAdUnitId = "ca-app-pub-3940256099942544/5224354917";
